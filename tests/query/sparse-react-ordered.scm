473ba98879ebdea6f403f25cd8f2aa65349644c0
Linas Vepštas <linasvepstas@gmail.com>
2022-07-29 13:55:36 -0500

    Add missing consraint

--- tests/query/sparse-react-ordered.scm
+++ tests/query/sparse-react-ordered.scm
@@ -56,6 +56,7 @@
 				(SBo (Variable "$hydroxO1") (Variable "hydroxy moiety"))
 				(Glob "rest of hydroxy")
 			)
+			(Not (Identical (Variable "$hydroxH1") (Variable "hydroxy moiety")))
 
 			; The above will match the OH's in both groups.
 			; They should be kept distinct.
