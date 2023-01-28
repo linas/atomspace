45b53db87afc7d6d8a04c331e9cf441ea708bd50
Linas Vepstas <linasvepstas@gmail.com>
2023-01-27 19:53:57 -0600

    Allocator with reports

--- opencog/alloc/Alloc.h
+++ opencog/alloc/Alloc.h
@@ -2,6 +2,9 @@
 #ifndef __ATOMSPACE_ALLOCATOR_H__
 #define __ATOMSPACE_ALLOCATOR_H__
 
+#include <atomic>
+#include <opencog/util/Logger.h>
+
 template <class T>
 class asallo
 {
@@ -20,14 +23,30 @@ public:
 	value_type*
 	allocate(std::size_t n)
 	{
+		nallocs++;
 		void * mem = malloc(n*sizeof(value_type));
 		nbytes_req += n*sizeof(value_type);
+		nbytes_use += malloc_usable_size(mem);
+
+		size_t na = nallocs.load();
+		size_t req = nbytes_req.load();
+		size_t use = nbytes_use.load();
+		if (0 == na % (1024*1024))
+		{
+			size_t nf = nfrees.load();
+			logger().info("Allo: nallo=%lu nfree=%lu diff=%lu req=%lu MiB use=%lu MiB avg = %lu",
+				na, nf, na-nf, req/(1024*1024), use/(1024*1024), use/(na-nf));
+		}
+
 		return static_cast<value_type*>(mem);
 	}
 
 	void
-	deallocate(value_type* p, std::size_t) noexcept
+	deallocate(value_type* p, std::size_t n) noexcept
 	{
+		nfrees++;
+		nbytes_req -= n*sizeof(value_type);
+		nbytes_use -= malloc_usable_size(p);
 		free(p);
 	}
 };
