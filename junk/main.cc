d6f79178ba1515a497b297f19ea02245adf5dd81
Linas Vepštas <linasvepstas@gmail.com>
2025-01-15 15:21:44 -0600

    restructure

--- junk/main.cc
+++ junk/main.cc
@@ -8,6 +8,34 @@
 
 void use_dev(cl::Device ocldev)
 {
+	std::string dname = ocldev.getInfo<CL_DEVICE_NAME>();
+	std::string dvers = ocldev.getInfo<CL_DEVICE_VERSION>();
+	printf("Using device %s\n", dname.c_str());
+	printf("\tVersion %s\n", dvers.c_str());
+
+#if 0
+	std::ifstream helloWorldFile("hello.cl");
+	std::string src(std::istreambuf_iterator<char>(helloWorldFile), (std::istreambuf_iterator<char>()));
+
+	cl::Program::Sources sources( 1, std::make_pair(src.c_str(), src.length() + 1));
+
+	cl::Context context(device);
+	cl::Program program(context, sources);
+
+	auto err = program.build("-cl-std=CL1.2");
+
+	char buf[16];
+	cl::Buffer memBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buf));
+	cl::Kernel kernel(program, "HelloWorld", &err);
+	kernel.setArg(0, memBuf);
+
+	cl::CommandQueue queue(context, device);
+	queue.enqueueTask(kernel);
+	queue.enqueueReadBuffer(memBuf, CL_TRUE, 0, sizeof(buf), buf);
+
+	std::cout << "hello";
+	std::cin.get();
+#endif
 }
 
 int main(int argc, char* argv[])
@@ -49,30 +77,7 @@ int main(int argc, char* argv[])
 		// if (pvers.find("OpenCL 3.") != std::string::npos)
 		// 	oclplat = plat;
 	}
+	printf("\n\n");
 
-#if 0
-	auto device = devices.front();
-
-	std::ifstream helloWorldFile("hello.cl");
-	std::string src(std::istreambuf_iterator<char>(helloWorldFile), (std::istreambuf_iterator<char>()));
-
-	cl::Program::Sources sources( 1, std::make_pair(src.c_str(), src.length() + 1));
-
-	cl::Context context(device);
-	cl::Program program(context, sources);
-
-	auto err = program.build("-cl-std=CL1.2");
-
-	char buf[16];
-	cl::Buffer memBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buf));
-	cl::Kernel kernel(program, "HelloWorld", &err);
-	kernel.setArg(0, memBuf);
-
-	cl::CommandQueue queue(context, device);
-	queue.enqueueTask(kernel);
-	queue.enqueueReadBuffer(memBuf, CL_TRUE, 0, sizeof(buf), buf);
-
-	std::cout << "hello";
-	std::cin.get();
-#endif
+	use_dev(ocldev);
 }
