3c8888c1b173eb6e9a8dcc166353c411468f826c
Linas Vepštas <linasvepstas@gmail.com>
2025-01-15 14:28:10 -0600

    Start work on basic

--- junk/main.cc
+++ junk/main.cc
@@ -5,38 +5,40 @@
 #include <iostream>
 #include <fstream>
 
-int main() 
+int main(int argc, char* argv[])
 {
-    std::vector<cl::Platform> platforms;
-    cl::Platform::get(&platforms);
+	std::vector<cl::Platform> platforms;
+	cl::Platform::get(&platforms);
 
-    auto platform = platforms.front();
-    std::vector<cl::Device> devices;
-    platform.getDevices(CL_DEVICE_TYPE_CPU, &devices);
+	printf("got %d plaforms\n", platforms.size());
 
-    auto device = devices.front();
+	auto platform = platforms.front();
+	std::vector<cl::Device> devices;
+	platform.getDevices(CL_DEVICE_TYPE_CPU, &devices);
+
+	auto device = devices.front();
 
 #if 0
-    std::ifstream helloWorldFile("hello.cl");
-    std::string src(std::istreambuf_iterator<char>(helloWorldFile), (std::istreambuf_iterator<char>()));
+	std::ifstream helloWorldFile("hello.cl");
+	std::string src(std::istreambuf_iterator<char>(helloWorldFile), (std::istreambuf_iterator<char>()));
 
-    cl::Program::Sources sources( 1, std::make_pair(src.c_str(), src.length() + 1));
+	cl::Program::Sources sources( 1, std::make_pair(src.c_str(), src.length() + 1));
 
-    cl::Context context(device);
-    cl::Program program(context, sources);
+	cl::Context context(device);
+	cl::Program program(context, sources);
 
-    auto err = program.build("-cl-std=CL1.2");
+	auto err = program.build("-cl-std=CL1.2");
 
-    char buf[16];
-    cl::Buffer memBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buf));
-    cl::Kernel kernel(program, "HelloWorld", &err);
-    kernel.setArg(0, memBuf);
+	char buf[16];
+	cl::Buffer memBuf(context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buf));
+	cl::Kernel kernel(program, "HelloWorld", &err);
+	kernel.setArg(0, memBuf);
 
-    cl::CommandQueue queue(context, device);
-    queue.enqueueTask(kernel);
-    queue.enqueueReadBuffer(memBuf, CL_TRUE, 0, sizeof(buf), buf);
+	cl::CommandQueue queue(context, device);
+	queue.enqueueTask(kernel);
+	queue.enqueueReadBuffer(memBuf, CL_TRUE, 0, sizeof(buf), buf);
 
-    std::cout << "hello";
-    std::cin.get();
+	std::cout << "hello";
+	std::cin.get();
 #endif
 }
