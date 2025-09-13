bc88c914e637f732d5cd8e0043b01a3c8f6da335
Linas Vepštas <linasvepstas@gmail.com>
2025-09-13 14:49:59 -0500

    More insane

--- insane-more-O2.cc
+++ insane-more-O2.cc
@@ -5,19 +5,19 @@
 
 // Bug: boolean values seem to be mis-handled with -O2 optimization.
 // Caution: yes, there are floats in this bug report. No, that's not
-// the problem, although I gess there is entanglement.
+// "really" the problem, although they are entangled into it.
 //
 // The following attempts to compare vectors of floats.
-// The compare is done by casting the floats to int64_t
+// The compare is done by casting the floats to int64_t.
 // Yes, there is reason to "hate this", but its not unusual.
 // At any rate, that's not what this bug is about.
 // The int64_t (and uint64_t)'s behave as expected.
 // A bool is computed. The printed value of the bool is correct.
-// Despite the bool evaluating to "true", the if-statement fails !!!!
-// The "if" test fails, repeatedly. !!!
+// Despite the bool being "true", the if-statement treats it as false.
+// And not just once, but consistently.
 //
-// g++ -O2 insane-O2.cc -o insane-O2-bad
-// g++ -O1 insane-O2.cc -o insane-O2-good
+// g++ -O2 insane-more-O2.cc -o insane-more-O2-bad -save-temps
+// g++ -O1 insane-more-O2.cc -o insane-more-O2-good
 //
 // Offending compiler, linker, runtime loader:
 // gcc (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0
@@ -36,7 +36,7 @@ bool is_it_big(int64_t lili)
 
 #define MAX_ULPS 24LL
 	bool yes_big = (MAX_ULPS < lulu);
-printf("duude lili=%lx lulu=%lx is it big? %d\n", lili, lulu, yes_big);
+printf("duude lili=0x%lx lulu=0x%lx is it big? %d\n", lili, lulu, yes_big);
 	return yes_big;
 }
 
@@ -61,9 +61,10 @@ bool equal_p(const std::vector<double>& self, const std::vector<double>& other)
 			llabs(*(int64_t*) &(self[i]) - *(int64_t*)&(other[i]));
 
 		bool toobig = is_it_big(lili);
-		printf("duuude %zu lili=%lx and ulps = %d\n", i, lili, toobig);
+		printf("duuude %zu lili=0x%lx and toobig = %d\n", i, lili, toobig);
 
 		if (toobig) return false;
+
 		printf("duuude %zu keep going because toobig = %d\n", i, toobig);
 		if (toobig) printf("wtf its too big\n");
 		else printf("keep going\n");
